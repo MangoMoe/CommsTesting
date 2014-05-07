e4e96acd1db39d7214a1346b78cf9a8b595ca496
drc95 <galzra@gmail.com>
2014-05-06 19:22:48 -0600

    Updating comms testing things
    HeaderType!!!!

--- src/testReceivePacket/Receiver.java
+++ src/testReceivePacket/Receiver.java
@@ -4,8 +4,13 @@ import java.io.IOException;
 
 public class Receiver 
 {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) /*throws IOException*/ {
+		try{
     	System.out.println(System.getProperty("user.dir") + "\nReciever.java reporting for duty.");
         new ReceiverThread().start();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("aaaaaah!");
+		}
     }
 }
